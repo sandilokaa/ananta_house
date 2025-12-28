b629e1458f400966bbaa85af38f00bb8c3dc5a77
sandilokaa <sandyloka790@gmail.com>
2025-12-28 13:38:41 +0700

    model: add category model

---
+++ lib/models/category_model.dart
@@ -0,0 +1,14 @@
+class Category {
+  String name;
+
+  Category({required this.name});
+}
+
+var categories = [
+  Category(name: 'Any Type'),
+  Category(name: 'Rent'),
+  Category(name: 'Sell'),
+  Category(name: 'House'),
+  Category(name: 'Villa'),
+  Category(name: 'Apartment'),
+];
